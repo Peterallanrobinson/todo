6b940751f1d9959dadfb688006ec6262634ffb93
Joseph Abell <joseph.abell@piksel.com>
2017-07-19 21:55:50 +0100

    adding first part of the reducer, adding ADD_TODO test for reducer

--- app/reducers/reducer.js
+++ app/reducers/reducer.js
@@ -1,1 +1,23 @@
+import * as actions from '../actions/actions.js';
 
+const initialState = {
+  date: Date.now(),
+  todoBeingEdited: null,
+  todos: []
+};
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case(actions.ADD_TODO):
+      state.todos.push({
+        todoText: action.todoText,
+        status: "incomplete"
+      });
+
+      return state;
+    default:
+      return state;
+  }
+}
+
+export default reducer;
---
+++ test/spec/reducers.js
@@ -0,0 +1,29 @@
+import jasmineEnzyme from 'jasmine-enzyme';
+import reducer from '../../app/reducers/reducer.js';
+import * as actions from '../../app/actions/actions.js';
+
+describe('Reducers:', () => {
+  beforeEach(() => {
+    jasmineEnzyme();
+  });
+
+  describe('ADD_TODO', function() {
+    it('should add a todo to the state', function() {
+      let state = {
+        todos: []
+      };
+
+      const result = reducer(state, actions.addTodo("Chicken Dinner"));
+
+      expect(result).toEqual({
+        todos: [
+          {
+            todoText: "Chicken Dinner",
+            status: "incomplete"
+          }
+        ]
+      });
+    });
+  });
+});
+
